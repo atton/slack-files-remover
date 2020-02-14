0add90334e91e5dc6ed00351e49f81ac5510a6c2
atton <e115763@gmail.com>
2020-02-15 06:44:57 +0900

    Add files-remover.rb

---
+++ README.md
@@ -0,0 +1,32 @@
+### Slack files remover
+* Delete files before 30 days
+
+## Base source code
+* https://gist.github.com/jamescmartinez/909401b19c0f779fc9c1
+
+## Slack API
+* [files.delete](https://api.slack.com/methods/files.delete)
+* [files.list](https://api.slack.com/methods/files.list)
+* Note: [all method](https://api.slack.com/methods)
+
+## How to Setup in Slack
+* Access [Apps of Slack](https://api.slack.com/apps) page
+* Create an App
+  * App Name: slack-files-remover(modifiable)
+  * Development Slack Workspace: your-work-space-name
+* Set Permissions and Install App
+  * Scopes > User Token Access > Add OAuth Scope
+    * Add 'files:read' and 'files:write'
+  * Click 'Install Apps to Workspace'
+    * Get OAuth Access Token and Set in 'files-remover.rb'.
+    * Note: Token starts with 'xoxp-'
+
+## How to Setup Gems
+* $ bundle install
+* $ echo 'TOKEN=xoxp-your-slack-token-hogehoge' > .env
+
+## How to execute
+* $ bundle exec ruby files-remover.rb
+
+## License
+* MIT
---
+++ files-remover.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require 'dotenv/load'
+require 'json'
+require 'net/http'
+require 'pry'
+require 'uri'
+
+@token = ENV['TOKEN']
+
+def list_files
+  ts_to = (Time.now - 30 * 24 * 60 * 60).to_i # 30 days ago
+  params = { token: @token, ts_to: ts_to, count: 50 }
+  uri = URI.parse('https://slack.com/api/files.list')
+  uri.query = URI.encode_www_form(params)
+  response = Net::HTTP.get_response(uri)
+  json = JSON.parse(response.body)
+  json['files']
+end
+
+def delete_files(file_ids)
+  file_ids.each do |file_id|
+    params = { token: @token, file: file_id }
+    uri = URI.parse('https://slack.com/api/files.delete')
+    uri.query = URI.encode_www_form(params)
+    response = Net::HTTP.get_response(uri)
+    p "#{file_id}: #{JSON.parse(response.body)['ok']}"
+  end
+end
+
+20.times do |i|
+  puts "count: #{i}"
+  files = list_files
+  file_ids = files.map { |f| f['id'] }
+  delete_files(file_ids)
+  num = 100 + (Random.rand * 50)
+  puts num
+  sleep num
+end
